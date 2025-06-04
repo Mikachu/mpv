b2f2b86e3b20b272224e43261f053edf3985bb04
Mikael Magnusson <mikachu@gmail.com>
2025-06-04 15:48:09 +0200

    Always show the playing filename in case the user produced a single filename via a glob

--- player/loadfile.c
+++ player/loadfile.c
@@ -1834,9 +1834,7 @@ static void play_current_file(struct MPContext *mpctx)
 
     handle_force_window(mpctx, false);
 
-    if (mpctx->playlist->num_entries > 1 ||
-        mpctx->playing->playlist_path)
-        MP_INFO(mpctx, "Playing: %s\n", mpctx->filename);
+    MP_INFO(mpctx, "Playing: %s\n", mpctx->filename);
 
     mp_assert(mpctx->demuxer == NULL);
 
